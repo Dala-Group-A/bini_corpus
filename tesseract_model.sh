975d21ea5a8efe5bd63e6893a346057324626175
ezeabasili edward <ezeabasilied15@gmail.com>
2025-08-22 09:51:59 +0100

    fix: issue with stripped characters
    the courier prime sans font couldn't render the
    unique bini characters so i had to change to noto-sans

--- tesseract_model.sh
+++ tesseract_model.sh
@@ -1,4 +1,4 @@
-#!/bash
+#!/bin/bash
 
 # install tesseract and others for linux
 # sudo apt install tesseract-ocr -y
@@ -8,24 +8,29 @@
 # sudo apt install training-tools -y # you'll have to build from source via
 
 # creating training images
+# courier prime sans cannot render the fonts hence noto sans for now is the way forward
 text2image \
-    --fonts_dir=/usr/share/fonts \
-    --font="Courier Prime Sans" \
-    --text=bini.training_text \
-    --outputbase=bini.exp0 \
-    # --lang=eng
+    --text tessa_data/bini.training_text \
+    --outputbase bini.exp0 \
+    --font "Noto Sans" \
+    --strip_unrenderable_words false \
+    --D ./tessa_data/ \
+    --U bini.unicharset \
+    --unicharset_file bini.unicharset \
+    --fonts_dir /usr/share/fonts \
+    
 
 # creating training data
 tesseract bini.exp0.tif bini.exp0 --psm 6 lstm.train
 
 # train with tesseract
 combine_lang_model \
-    --input_dir . \
     --lang bini \
     --output_dir /. \
+    --training_text bini.training_text \
+    --wordlist bini.training_wordlist \
+    --input_unicharset bini.unicharset \
+    --fonts_dir /usr/share/fonts \
     --langdata_dir /usr/share/tesseract-ocr/4.00/tessdata \
     --tessdata_dir /usr/share/tesseract-ocr/4.00/tessdata_best \
     --script_dir /usr/share/tesseract-ocr/4.00/tessdata \
-    --training_text bini.training_text \
-    --wordlist bini.training_wordlist \
-    --fonts_dir /usr/share/fonts
